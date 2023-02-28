6119deb7d72d6ab6ff8a4d93d28568062bb477ce
sparrro <116090214+sparrro@users.noreply.github.com>
2023-02-28 15:40:28 +0100

    hope it doesnt fuck up

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // funktion som loopar över alternativen och returnerar högsta
 
-function findHighestVOte() {
+function findHighestVOte(array) {
   let maxVotes = 0;
   let result;
 
-  for (let restaurant of restaurants) {
+  for (let restaurant of array) {
     if (restaurant.votes > maxVotes) {
       maxVotes = restaurant.votes;
     }
   }
 
-  result = restaurants.find((restaurant) => restaurant.votes === maxVotes);
+  result = array.find((restaurant) => restaurant.votes === maxVotes);
   console.log(result);
+  return result
 }
+
+
+let restaurantResult = findHighestVOte(restaurants)
