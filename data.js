09f1650a4ed1528a862e5c19346c84c1d1e586b0
Joakim Sundman <82515307+jayhyllie@users.noreply.github.com>
2023-03-01 14:28:27 +0100

    Finished
    Co-authored-by: larssonrd <larssonrd@users.noreply.github.com>
    Co-authored-by: sparrro <sparrro@users.noreply.github.com>

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "Il Tavolo",
-    adress: "Storgatan 12, 123 45 Stockholm",
+    address: "Storgatan 12, 123 45 Stockholm",
     foodType: "Italiensk",
     url: "https://tavolo.se/",
     openingHours: "Mån-Sön 11:00-23:00",
@@ -11,7 +11,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "Bastard's BBQ",
-    adress: "Parkvägen 7, 543 21 Göteborg",
+    address: "Parkvägen 7, 543 21 Göteborg",
     foodType: "Amerikansk",
     url: "https://bbqsteakhouse.se/",
     openingHours: "Mån-Fre 11:00-22:00, Lör-Sön 11:00-23:00",
@@ -20,7 +20,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "Qiao Mei",
-    adress: "Strandgatan 4, 456 78 Malmö",
+    address: "Strandgatan 4, 456 78 Malmö",
     foodType: "Kinesisk",
     url: "http://lai-wa.se/",
     openingHours: "Mån-Sön 12:00-22:00",
@@ -29,7 +29,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "El Diablo del Taco",
-    adress: "Södra Kyrkogatan 2, 987 65 Uppsala",
+    address: "Södra Kyrkogatan 2, 987 65 Uppsala",
     foodType: "Mexikansk",
     url: "https://tacobar.se/",
     openingHours: "Mån-Sön 11:00-23:00",
@@ -38,7 +38,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "Thai House",
-    adress: "Norra Vägen 14, 654 32 Västerås",
+    address: "Norra Vägen 14, 654 32 Västerås",
     foodType: "Thailändsk",
     url: "http://thaihousevasa.se/",
     openingHours: "Mån-Sön 11:30-22:00",
@@ -47,7 +47,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "La Bise",
-    adress: "Hamngatan 8, 321 09 Sundsvall",
+    address: "Hamngatan 8, 321 09 Sundsvall",
     foodType: "Fransk",
     url: "https://bise.se/",
     openingHours: "Mån-Sön 18:00-23:00",
@@ -56,7 +56,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "Bombay",
-    adress: "Östra Storgatan 20, 567 89 Linköping",
+    address: "Östra Storgatan 20, 567 89 Linköping",
     foodType: "Indisk",
     url: "http://jarntorget.restaurangbombay.se/",
     openingHours: "Mån-Sön 12:00-22:30",
@@ -65,7 +65,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "Papadopoulos Grill&Salladsbar",
-    adress: "Kungsgatan 3, 890 12 Örnsköldsvik",
+    address: "Kungsgatan 3, 890 12 Örnsköldsvik",
     foodType: "Grekisk",
     url: "https://restaurangathena.se/",
     openingHours: "Mån-Sön 11:00-23:00",
@@ -74,7 +74,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "Mammas Sushi",
-    adress: "Södergatan 16, 234 56 Helsingborg",
+    address: "Södergatan 16, 234 56 Helsingborg",
     foodType: "Japansk",
     url: "http://www.sushikaze.se/",
     openingHours: "Mån-Sön 11:30-22:00",
@@ -83,7 +83,7 @@ export const restaurants = [
   {
     imgUrl: "https://source.unsplash.com/random/800x600/?restaurant",
     name: "Gunnar's",
-    adress: "Köpmangatan 5, 789 01 Jönköping",
+    address: "Köpmangatan 5, 789 01 Jönköping",
     foodType: "Svensk",
     url: "https://www.gunnarskok.se/",
     openingHours: "Mån-Sön 11:00-21:00",
--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+import {restaurants, activityFoodPlaces} from "./data.js"
+
 const primaryNav = document.querySelector('.navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
 navToggle.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute('data-visible');
-    
-    if(visibility === 'false') {
-        primaryNav.setAttribute('data-visible', true)
-        navToggle.setAttribute('aria-expanded', true)
-    } else {
-        primaryNav.setAttribute('data-visible', false)
-        navToggle.setAttribute('aria-expanded', false)
-    }
+  const visibility = primaryNav.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', true)
+    navToggle.setAttribute('aria-expanded', true)
+  } else {
+    primaryNav.setAttribute('data-visible', false)
+    navToggle.setAttribute('aria-expanded', false)
+  }
+})
+
+//Event listeners
+const foodBtn = document.getElementById("food-btn")
+const activityBtn = document.getElementById("activity-btn")
+
+foodBtn.addEventListener("click", function() {
+  renderAlternativeCards(restaurants)
+})
+
+activityBtn.addEventListener("click", function() {
+  renderAlternativeCards(activityFoodPlaces)
 })
 
 
@@ -29,21 +43,100 @@ function findHighestVOte(array) {
   return result
 }
 
-//funktion som renderar ut korten och sätter eventlystnare på dem
-function renderCards(array) {
-  array.forEach(alternative => {
-    //funktion som tar ett objekt och returnerar ett html-element
-    let cardEl = makeAlternativeCard(card)
+const winnerBtn = document.getElementById("winner-btn")
 
-    //lägg till eventlystnare på cardEL och lägg in i ui:n
+winnerBtn.addEventListener("click", function(e) {
+  renderWinner(activityFoodPlaces)
+  primaryNav.setAttribute('data-visible', false)
+  navToggle.setAttribute('aria-expanded', false)
+
+})
+
+function renderWinner(array) {
+  const container = document.querySelector(".main")
+  const winner = findHighestVOte(array);
+  container.innerHTML = `
+        <article class="vote__winner">
+          <img src="${winner.imgUrl}" alt="" class="winner--image">
+          <section class="winner">
+            <h2 class="winner--name">${winner.name}</h2>
+            <p class="winner-address">${winner.address}</p>
+            <h3 class="winner--food">${winner.foodType}</h3>
+            ${winner.activityType ? `<p class="info--activity">${winner.activityType}</p>` : ""}
+            <a href="${winner.url}" class="winner--web">Hemsida</a>
+            <p class="winner--opening">${winner.openingHours}</p>
+            <span class="winner--direction">Vägbeskrivning</span>
+          </section>
+        </article>
+  `
+}
+
+
+function renderAlternativeCards(array) {
+  const mainContainer = document.querySelector(".main")
+  mainContainer.innerHTML = ``
+  array.forEach(venue => {
+    const cardEl = document.createElement('article')
+    cardEl.classList.add("main__card")
+    cardEl.innerHTML = `
+        <img src="${venue.imgUrl}" alt="" class="card--image">
+    <section class='main__card--info'>
+      <h2 class="info--name">${venue.name}</h2>
+      <p class="info--address"></p>
+      ${venue.activityType ? `<p class="info--activity">${venue.activityType}</p>` : ""}
+      <h3 class="info--food">${venue.foodType}</h3>
+      <a href="${venue.url}" class="info--web">Gå till hemsida</a>
+      <p class="info--opening">${venue.openingHours}</p>
+      <button>Välj</button>
+    </section>
+    `
+    mainContainer.insertAdjacentElement("afterbegin", cardEl)
+    //eventlystnare på cardEl
+    const voteButton = cardEl.querySelector("button")
+    cardEl.addEventListener('click', function(e) {
+      console.log("testtest")
+    if(e.target === voteButton) {
+      venue.votes++
+      renderVoteChart()
+      console.log(venue.votes)
+    }
+    })
   })
 }
 
-function makeAlternativeCard(card) {
-  let cardEl = document.createElement(/*vad det nu ska vara*/)
-  cardEl.innerHTML = ``
-  cardEl.classList.add() //eventuellt också på dess children
-  return cardEL
-}
+// renderAlternativeCards(restaurants)
 
-//hallå hallå
+
+
+function renderVoteChart() {
+  document.querySelector('.main').innerHTML = ''
+  const combinedArray = restaurants.concat(activityFoodPlaces)
+  
+  //hitta top 5 i kombinerade arrayen
+  const winnersArray = [];
+  for (let i = 0; i< 5; i++) {
+    //kammar igenom kombinerade arrayen 5 gånger och flyttar toppalternativet till vinnararrayen
+    let highest = findHighestVOte(combinedArray);
+    winnersArray.push(highest);
+    combinedArray.splice(combinedArray.indexOf(highest), 1)
+  }
+
+  //renderar ut vinnararrayen
+  const winnersArtEl = document.createElement('article');
+  winnersArtEl.classList.add('vote__stats');
+  const winnersUlEl = document.createElement('ul');
+  winnersUlEl.classList.add('vote__stats--list');
+  winnersUlEl.setAttribute('role', 'list');
+
+  winnersArray.forEach(winner => {
+    const voteCountEl = document.createElement('li');
+    voteCountEl.classList.add('vote__stats--list-item');
+    voteCountEl.innerHTML = `
+      <span class="voteResult" style="height: ${winner.votes * 5}"></span>
+      <h4 class="vote--venue">${winner.name}</h4>`;
+    winnersUlEl.appendChild(voteCountEl);
+  });
+  winnersArtEl.appendChild(winnersUlEl);
+  
+  document.querySelector('.main').appendChild(winnersArtEl)
+}
